33bc688de22a94773944e6b70bbf1ff33c3b0f4d
Jordan Bott <jbott@clerk.dev>
2026-02-17 12:39:18 -0800

    testing deployment

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -14,7 +14,7 @@ export default function HomePage() {
       <SignedOut>
         <SignInButton />
       </SignedOut>
-      <p>On the homepage!</p>
+      <p>On the homepage! :)</p>
     </div>
   );
 }
